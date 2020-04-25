3bf30a377c9065c6ce3a169d23a1af6d97faf91b
MW <marian.witkowski@gmail.com>
2020-04-25 17:01:20 +0200

    major changes

---
+++ MiniShop/Order.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    public class Order
+    {
+
+        // typ wyliczeniowy
+        enum OrderStatus
+        {
+            NewOrder = 10,
+            Complete = 20,
+            Confirmed = 30,
+            Shipped = 40,
+            Returned = 50,
+            Cancelled = 60
+        }
+
+        private String orderNumber;
+        private List<OrderItem> items = new List<OrderItem>(); //utworzona pusta lista
+        private byte discount = 0;
+        private String customerName;
+        private String shipAddress;
+        private DateTime orderDate;
+        private OrderStatus status = OrderStatus.NewOrder;
+
+        public double TotalAmount
+        {
+            get { return CalcTotalAmount(); }
+        }
+
+        //metody
+        public bool AddProduct(Product product, int qnty)
+        {
+            //akcja dodania produktu do listy produktow
+            if (status == OrderStatus.NewOrder && qnty > 0 && product != null)
+            {
+                items.Add(new OrderItem(product, qnty));
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public bool Clear()
+        {
+            if (status == OrderStatus.NewOrder)
+            {
+                items.Clear();
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        private double CalcTotalAmount()
+        {
+            double amount = 0.0;
+            foreach (var item in items)
+            {
+                amount += item.ProductPrice * item.Qnty;
+            }
+            if (discount>0 && discount<=100)
+            {
+                amount *= (100 - discount) / 100.0;
+            }
+            return amount;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Elementy zamówienia:");
+            foreach (var item in items)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}", 
+                    "", item.Qnty, item.ProductPrice, item.ProductPrice*item.Qnty);
+            }
+            Console.WriteLine("Do zapłaty: {0}", CalcTotalAmount() );
+        }
+
+    }
+}
---
+++ MiniShop/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    public class Product
+    {
+        private int id;
+        private String name;
+        private double price;
+        private String descr = String.Empty; // ""
+        private UInt16 version = 0;
+        private Boolean promo = false;
+        private Boolean active = true;
+
+        public double Price { get { return price; } }
+
+        // ustawianie parametrow
+        public void SetParam(int id, String name, double price, String descr="")
+        {
+            //
+        }
+
+        // zmiana ceny
+        public void ChangePrice(double newPrice)
+        {
+            // zastanowic sięnad mechanizmem logowania historii zmian cen
+        }
+
+        // zmiana opisu
+        public void ChangeDescription(String newDescr)
+        {
+
+        }
+
+        public void SetActive(bool isActive)
+        {
+            this.active = isActive;
+        }
+
+
+    }
+}
