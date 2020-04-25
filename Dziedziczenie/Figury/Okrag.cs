3caeffe37ee7a7ee916e1c06b761c8ab8e7c702f
alx-csharp0404 <64251407+alx-csharp0404@users.noreply.github.com>
2020-04-25 14:26:17 +0200

    Update Okrag.cs

--- Dziedziczenie/Figury/Okrag.cs
+++ Dziedziczenie/Figury/Okrag.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//zmiana z poziomu Githuba
 namespace Dziedziczenie.Figury
 {
     class Okrag
     {
         private double srednica;
+        
+        public double ObliczPole() {
+            return 3.14159 * (0.5*srednia) * (0.5*srednia);
+        }
     }
 }
